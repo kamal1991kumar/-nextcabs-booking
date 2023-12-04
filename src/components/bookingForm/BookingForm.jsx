6a76cb7c8ba98794992ae5ab07cf2f9ef786e1c7
kamal <kamal1991kumar@gmail.com>
2023-12-04 15:03:47 +0530

    UI change

--- src/components/bookingForm/BookingForm.jsx
+++ src/components/bookingForm/BookingForm.jsx
@@ -3,17 +3,15 @@ import React from 'react';
 export default function BookingForm() {
     return (
         <div className='bookingContainer'>
-            <div className='container'>
-                <div className='row'>
-                    <h5 className='bookingContainer__tab col text-center p-3 mb-0'>Airport Transfers</h5>
+            <div className='row'>
+                    <div className='col'><h5 className='bookingContainer__tab text-center p-3 mb-0'>Airport Transfers</h5></div>
                     <div className='col'>&nbsp;</div>
                 </div>
-            </div>
             <div className='bookingContainer__box px-2 py-4'>
                 <div className='container py-2'>
                     <div className='row'>
                         <div className='col-12 col-lg-6'>
-                            <fieldset class="border rounded-3 px-3 py-2 mb-4">
+                            <fieldset class="border rounded-3 px-3 py-2 mb-4 border-danger text-danger">
                                 <legend class="float-none w-auto px-2 mb-0"><h6 className='mb-0'>Drop-off</h6></legend>
                                 <input type='text' className='border-0 p-0 w-100' />
                             </fieldset>
@@ -31,28 +29,22 @@ export default function BookingForm() {
                         <div className='col-12  col-lg-6'>
                             <div className='d-flex'>
                                 <fieldset class=" w-100 border rounded-start px-3 py-2 mb-4">
-                                    <legend class="float-none w-auto px-2 mb-0"><h6 className='mb-0'>Drop-off</h6></legend>
+                                    <legend class="float-none w-auto px-2 mb-0"><h6 className='mb-0'>Date</h6></legend>
                                     <input type='date' className='border-0 p-0 w-100' />
                                 </fieldset>
                                 <fieldset class="w-100 border rounded-end  px-3 py-2 mb-4">
-                                    <legend class="float-none w-auto px-2 mb-0"><h6 className='mb-0'>Drop-off</h6></legend>
+                                    <legend class="float-none w-auto px-2 mb-0"><h6 className='mb-0'>Time</h6></legend>
                                     <input type='time' className='border-0 p-0 w-100' />
                                 </fieldset>
                             </div>
                         </div>
-                        <div className='col-12  col-lg-6'>
-                            <div className='d-flex'>
-                                <fieldset class=" w-100 border rounded-start px-3 py-2 mb-4">
-                                    <legend class="float-none w-auto px-2 mb-0"><h6 className='mb-0'>Drop-off</h6></legend>
-                                    <input type='date' className='border-0 p-0 w-100' />
-                                </fieldset>
-                                <fieldset class="w-100 border rounded-end  px-3 py-2 mb-4">
-                                    <legend class="float-none w-auto px-2 mb-0"><h6 className='mb-0'>Drop-off</h6></legend>
-                                    <input type='time' className='border-0 p-0 w-100' />
-                                </fieldset>
-                            </div>
+                        <div className='col-12  col-lg-6 pt-2 mb-4'>
+                            <button className='border btn btn-outline-secondary w-100 p-2'><span className='p-1 d-inline-block'>Add Return</span></button>
                         </div>
                     </div>
+                </div>
+                <div className='d-flex justify-content-end pe-3 me-2 mb-2'>
+                <button className='btn btn-primary btn-lg py-3 px-5'>Search</button>
                 </div>
             </div>
         </div>
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -4,11 +4,20 @@ import BaseLayout from 'layout/baseLayout/index';
 export default function BookingPage() {
     return (
         <BaseLayout>
-            <div className='showcaseBox py-2 py-lg-5'>
+            <div className='showcaseBox py-2 py-lg-4'>
                 <div className='container'>
-                    <div className='row justify-content-md-center'>
-                        <div className='col-xl-8 col-sm-10 col-xs-12'>
+                    <div className='row justify-content-lg-center'>
+                        <div className='col-xl-8 col-12 mt-lg-3 py-5'>
                             <BookingForm />
+                        </div>
+                    </div>
+                </div>
+                
+                <div className='container'>
+                    <div className='row  justify-content-lg-center'>
+                        <div className='col-xl-6 col-12 py-3 text-center text-white'>
+                            <h3>Book your stress-free airport transfer today and save up to 40%</h3>
+                            <p>We compare airport transfer prices from hundreds of suppliers worldwide to offer you the lowest possible price without compromising the quality of service</p>
                         </div>
                     </div>
                 </div>
